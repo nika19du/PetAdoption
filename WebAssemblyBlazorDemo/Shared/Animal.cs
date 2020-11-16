4b84b3088f27682a0f6a6aec95033e162a65de07
nika19du <qnkovanikol29@gmail.com>
2020-11-16 19:07:08 +0200

    Add project files.

---
+++ WebAssemblyBlazorDemo/Shared/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAssemblyBlazorDemo.Shared
+{
+    public class Animal
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public int Age
+        {
+            get 
+            {
+                if (DateOfBirth.HasValue)
+                {
+                    var calculateAge = DateTime.Today.Year - DateOfBirth.Value.Year;
+
+                    if (DateTime.Today.DayOfYear< DateOfBirth.Value.DayOfYear)
+                    {
+                        calculateAge -= 1;
+                    }
+                    return calculateAge;
+                }
+                return EstimatedAge.Value;
+            } 
+        }
+
+        public int? EstimatedAge { get; set; }
+
+        public AnimalKind AnimalKind { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public Gender Gender { get; set; }
+
+    }
+}
---
+++ WebAssemblyBlazorDemo/Shared/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAssemblyBlazorDemo.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int VatPercentage { get; set; }
+
+        public decimal NetPrice => Price * 
+            (1+(VatPercentage/(decimal)100));
+
+        public string ProductImageUrl { get; set; }
+    }
+}
